545e2cfe9feef2977ff57abdefe25654ac445611
rodrfct <rodrigoca.fct@gmail.com>
2023-09-28 12:01:19 +0200

    Add lightbox

--- src/components/Lightbox.vue
+++ src/components/Lightbox.vue
@@ -1,18 +1,124 @@
 <script setup lang="ts">
 defineProps<{
-    img1: string,
-    img2: string,
-    img3: string,
-    img4: string
+    images: string[]
 }>()
+
+const activeImg = ref<number>(0)
+
+const modal = ref<HTMLDialogElement | null>(null)
 </script>
 
 <template>
     <div class="lightbox">
-        aa
+        <img :src="images[activeImg]" alt="" @click="modal?.showModal">
+        <div class="selector">
+            <img v-for="(img, index) in images" :src="img" alt=""
+            :class="{active : index == activeImg}"
+            @click="activeImg = index">
+        </div>
+
+        <dialog ref="modal">
+            <form method="dialog">
+                <button><img src="~/assets/icons/icon-close.svg" alt=""></button>
+            </form>
+
+            <div class="slider">
+                <img class="arrow"
+                src="~/assets/icons/icon-previous.svg" alt=""
+                @click="() => {if(activeImg > 0) {activeImg--}}">
+                <img :src="images[activeImg]" alt="">
+                <img class="arrow"
+                src="~/assets/icons/icon-next.svg" alt=""
+                @click="() => {if(activeImg < 3) {activeImg++}}">
+            </div>
+            <div class="selector">
+                <img v-for="(img, index) in images" :src="img" alt=""
+                :class="{active : index == activeImg}"
+                @click="activeImg = index">
+            </div>
+        </dialog>
     </div>
 </template>
 
 <style>
+.lightbox img {
+    aspect-ratio: 1/1;
+    border-radius: 10px;
+    max-width: 100%;
+    cursor: pointer;
 
+    .selector &:hover,
+    &.active {
+        filter: contrast(1.2) brightness(1.1);
+    }
+
+    &.active {
+        border: 2px solid var(--Orange);
+    }
+}
+
+.selector {
+    display: grid;
+    grid-template: 1fr / repeat(4, 1fr);
+
+    --margin: 20px;
+    gap: var(--margin);
+    margin: var(--margin) 0;
+}
+
+.lightbox dialog {
+    max-width: 60%;
+    border: none;
+    background-color: inherit;
+
+    & form {
+        display: flex;
+        justify-content: flex-end;
+        
+        > button {
+            border: none;
+            background-color: inherit;
+            
+            > img {
+                color: var(--Orange);
+            }
+        }
+    }
+
+    .selector {
+        margin: var(--margin);
+    }
+
+    &::backdrop {
+        background-color: black;
+        opacity: .65;
+    }
+}
+
+.slider {
+    position: relative;
+
+    .arrow {
+        position: absolute;
+        top: 50%;
+        
+        height: 10px;
+        padding: 10px;
+        background-color: white;
+        border: 1px solid var(--Orange);
+        border-radius: 50%;
+
+        &:first-child {
+            translate: -50% -50%;
+            right: 100%;
+            left: 0;
+        }
+        
+        &:last-child {
+            translate: -50% -50%;
+            left: 100%;
+            right: 0;
+        }
+    }
+}
 </style>
--- src/pages/products/sneakers.vue
+++ src/pages/products/sneakers.vue
@@ -2,6 +2,11 @@
 import Lightbox from '~/components/Lightbox.vue';
 import ProductInfo from '~/components/ProductInfo.vue';
 
+import img1 from "~/assets/images/image-product-1.jpg";
+import img2 from "~/assets/images/image-product-2.jpg";
+import img3 from "~/assets/images/image-product-3.jpg";
+import img4 from"~/assets/images/image-product-4.jpg";
+
 definePageMeta({
     layout: 'product'
 })
@@ -14,14 +19,23 @@ const sneakers = {
     price: 250,
     discount: 50,
 }
+
+const images: string[] = [
+    img1,
+    img2,
+    img3,
+    img4
+]
 </script>
 
 <template>
-    <Lightbox
-    img1="~/assets/images/image-product-1.png"
-    img2="~/assets/images/image-product-2.png"
-    img3="~/assets/images/image-product-3.png"
-    img4="~/assets/images/image-product-4.png" />
+    <!-- <Lightbox
+    img1="~/assets/images/image-product-1"
+    img2="~/assets/images/image-product-2"
+    img3="~/assets/images/image-product-3"
+    img4="~/assets/images/image-product-4" /> -->
+
+    <Lightbox :images="images" />
 
     <ProductInfo v-bind="sneakers" />
 </template>
