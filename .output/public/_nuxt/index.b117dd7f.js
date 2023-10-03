221cc74e292dfad8458694081dcec3fb0b970f64
rodrfct <rodrigoca.fct@gmail.com>
2023-10-03 11:13:20 +0200

    Revert "Add deployment"
    This reverts commit 73ef9528f3e67e227ad8477d79728032c675fcf5.

--- .output/public/_nuxt/index.b117dd7f.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./nuxt-link.f30254cb.js";import{_ as o,c as a,b as n,w as s,F as c,a as r,o as _,d}from"./entry.fae5aec6.js";const m={},l=r("h1",null,"Home Page",-1);function u(f,i){const e=t;return _(),a(c,null,[l,n(e,{to:"products/sneakers"},{default:s(()=>[d("Sneakers")]),_:1})],64)}const k=o(m,[["render",u]]);export{k as default};
